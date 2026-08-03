f198c11ea66d0ca853f8cffcf95f753877c3bc54
1073846661 <19988572308@163.com>
2026-08-03 17:20:19 +0800

    init: spring boot project scaffold

---
+++ src/main/java/com/iot/platform/IotDataPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.iot.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDataPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDataPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot/platform/IotDataPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDataPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
